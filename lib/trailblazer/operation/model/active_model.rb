5a8849988af9522ddbdc8e11c78c7b8fad6cc362
Timo Schilling <timo@schilling.io>
2015-09-25 16:14:34 +0200

    fix model concept api

--- lib/trailblazer/operation/model/active_model.rb
+++ lib/trailblazer/operation/model/active_model.rb
@@ -2,7 +2,7 @@ module Trailblazer
   module Operation::Model
     # Automatically set model_name on operation's contract.
     module ActiveModel
-      def contract(&block)
+      def contract(constant=nil, &block)
         super
         contract_class.model(model_class) # this assumes that Form::ActiveModel is mixed in.
       end
