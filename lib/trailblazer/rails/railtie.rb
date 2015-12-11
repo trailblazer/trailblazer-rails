e1d9546a9414770e29763e1fe36bcb31d9820d16
Nick Gorbikoff <nick.gorbikoff@gmail.com>
2015-12-11 11:45:44 -0600

    simplifying load process & cleaning it up & fixing loading in root app

--- lib/trailblazer/rails/railtie.rb
+++ lib/trailblazer/rails/railtie.rb
@@ -11,58 +11,36 @@ module Trailblazer
       # app_root/app/concepts/comments/opertaion/special_operation.rb
       # app_root/app/concepts/comments/opertaion/comment_operation.rb
 
-      # Picks up everything in the root of concepts, ie app/concepts/app_cell.rb - essentially classes that all concepts can inherit from.
-      files = Dir.glob("#{app.root}/app/concepts/*{#{items}}.rb")
+      files = []
+
+      # Picks up everything in the root of concepts, ie app/concepts/app_cell.rb
+      # - essentially classes that all concepts can inherit from.
+      # So all top level items such will get loaded as long as file name includes _item.rb
+      # Like app/concepts/app_cell.rb or app/concepts/default_customer_cell.rb
+      files = files | Dir[app.root.join('app', 'concepts',"*{#{items}}.rb")]
+
       # Picks up in the root of the concept - like app/concepts/comments/operation.rb
-      files = files | Dir.glob("#{app.root}/app/concepts/*{#{items}}.rb")
+      files = files | Dir[app.root.join('app', 'concepts','*', "{#{items}}.rb")]
+
       # Picks up everything in the root appropriate folder ie app/concepts/comments/operation/operation.rb
       # This will make sure to load operation.rb before  create.rb - helpful with cells
       # When  you have app/concepts/cell/cell.rb & app/concepts/cell/apple.rb and cell.rb needs to be loaded first.
-      files = files |  Dir.glob("#{app.root}/app/concepts/**/{#{items}}/{#{items}}.rb")
+      files = files | Dir[app.root.join('app', 'concepts','*', "{#{items}}","{#{items}}.rb")]
+
       # Pick up everything else in the item folder operations/create.rb or opertations/update.rb - except operation.rb
-      files = files |  Dir.glob("#{app.root}/app/concepts/**/{#{items}}/**.rb")
+      files = files | Dir[app.root.join('app', 'concepts','*', "{#{items}}",'*.rb')]
+
       # FIXME
       # Address a case when there is deeper nesting - thou it's too much
       # like app/concepts/comments/special_comments/cell/form.rb  -?
-      
+
       # Require all
       files.each do |f|
          require_dependency "#{f}"
       end
-      # Add to Rails autload paths
-      app.config.autoload_paths += files
     end
 
-    def self.autoload_operations(app)
-      self.autoload_items(app,'operation,operations')
-    end
 
-    def self.autoload_models(app)
-      self.autoload_items(app,'model,models')
-    end
-
-    def self.autoload_cells(app)
-      self.autoload_items(app,'cell,cells')
-    end
-
-    def self.autoload_contracts(app)
-      # Some people call them contracts, some people call them forms - It's Reform object.
-      self.autoload_items(app,'contract,contracts,forms,form')
-    end
-
-    def self.autoload_callbacks(app)
-      self.autoload_items(app,'callback,callbacks')
-    end
-
-     def self.autoload_policies(app)
-      self.autoload_items(app,'policy,policies')
-    end
-
-    def self.autoload_representers(app)
-      # Same idea as autoload_operations - no need for duplicate comments.
-      # Some people refer to them as decorators
-      self.autoload_items(app,'representers,decorators,representer,decorator')
-    end
 
     # This is to autoload Operation::Dispatch, etc. I'm simply assuming people find this helpful in Rails.
     initializer "trailblazer.library_autoloading" do
@@ -74,17 +52,13 @@ module Trailblazer
       # the trb autoloading has to be run after initializers have been loaded, so we can tweak inclusion of features in
       # initializers.
       ActionDispatch::Reloader.to_prepare do
-        # FIXME
-        # Down the line this can be changed to just this
-        # Trailblazer::Railtie.autoload_items(app,'operation,operations')
-        # For now let's keep it explicit, if case it needs to be overwriden
-        Trailblazer::Railtie.autoload_models(app)
-        Trailblazer::Railtie.autoload_policies(app)
-        Trailblazer::Railtie.autoload_representers(app)
-        Trailblazer::Railtie.autoload_contracts(app)
-        Trailblazer::Railtie.autoload_callbacks(app)
-        Trailblazer::Railtie.autoload_operations(app)
-        Trailblazer::Railtie.autoload_cells(app)
+        Trailblazer::Railtie.autoload_items(app,'model')
+        Trailblazer::Railtie.autoload_items(app,'policy')
+        Trailblazer::Railtie.autoload_items(app,'representer')
+        Trailblazer::Railtie.autoload_items(app,'contract,form')
+        Trailblazer::Railtie.autoload_items(app,'callback')
+        Trailblazer::Railtie.autoload_items(app,'operation')
+        Trailblazer::Railtie.autoload_items(app,'cell,cells')
       end
     end
   end
