706d51114e7a84632e67442a19de1fb987a34ed4
Nils Landt <nils@promisedlandt.de>
2017-01-02 12:03:47 +0100

    Fix controller cell namespacing

--- lib/trailblazer/rails/controller/cell.rb
+++ lib/trailblazer/rails/controller/cell.rb
@@ -1,24 +1,21 @@
-module Trailblazer::Rails::Cell
-  module Controller
-
+module Trailblazer::Rails::Controller::Cell
   private
 
-    module Render
-      def render(cell=nil, options={}, *, &block)
-        return super unless cell.kind_of?(::Cell::ViewModel)
-        render_cell(cell, options)
-      end
-
-      def render_cell(cell, options)
-        options = options.reverse_merge(layout: true)
-
-        # render the cell.
-        content = cell.()
-
-        render( { html: content }.merge(options) )
-      end
+  module Render
+    def render(cell=nil, options={}, *, &block)
+      return super unless cell.kind_of?(::Cell::ViewModel)
+      render_cell(cell, options)
     end
 
-    include Render
+    def render_cell(cell, options)
+      options = options.reverse_merge(layout: true)
+
+      # render the cell.
+      content = cell.()
+
+      render( { html: content }.merge(options) )
+    end
   end
+
+  include Render
 end
