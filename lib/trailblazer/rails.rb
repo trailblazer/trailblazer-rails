1398b565aac7ccefd620a33417b1cd8497ec3ea8
Nick Sutterer <apotonick@gmail.com>
2017-01-05 11:00:33 +0800

    no need for controller/ subdir as everything in this gem is controller-focused.

--- lib/trailblazer/rails.rb
+++ lib/trailblazer/rails.rb
@@ -8,7 +8,7 @@ end
 require "reform/rails"
 
 require "trailblazer/rails/controller"
-require "trailblazer/rails/controller/cell"
+require "trailblazer/rails/cell"
 require "trailblazer/rails/form"
 require "trailblazer/rails/railtie"
 require "trailblazer"
