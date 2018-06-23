06550a842c7e208248646df185accc3abbb303a1
Emanuele Magliozzi <emanuele.magliozzi@gmail.com>
2018-06-24 07:32:03 +0800

    Test added for _assign_trb_ivars and _wrap_with_trb_form

--- test/rails5.2/app/controllers/args_controller.rb
+++ test/rails5.2/app/controllers/args_controller.rb
@@ -3,7 +3,18 @@ class ArgsController < ApplicationController
     run Params::WithArgs, params:{ fake: "bla" }
   end
 
+  private
+
   def _run_options(context)
     context.merge({ :current_user => Module })
+  end
+
+  def _assign_trb_ivars(_result)
+    super
+    @model = 'my_model'
+  end
+
+  def _wrap_with_trb_form(_form, _model)
+    'my_form'
   end
 end
