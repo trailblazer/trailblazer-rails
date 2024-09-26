e155afefb3d8cbcd867419f3f8b4471689812afa
Nick Sutterer <apotonick@gmail.com>
2024-09-26 14:49:37 +0200

    in zeitwerk, check if we got a class constant before testing if it's an operation.

--- lib/trailblazer/rails/zeitwerk.rb
+++ lib/trailblazer/rails/zeitwerk.rb
@@ -3,7 +3,7 @@ require "trailblazer/activity/dsl/linear/zeitwerk"
 Trailblazer::Operation.extend(Trailblazer::Activity::DSL::Linear::Zeitwerk::Strategy)
 
 Rails.autoloaders.main.on_load do |_cpath, value, _abspath|
-  if value < Trailblazer::Operation
+  if value.is_a?(Class) && value < Trailblazer::Operation
     puts "==> #{value}"
 
     value.finalize! # compile the {Activity}.
